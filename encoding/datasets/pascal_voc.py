e4e37db7c959ed48d9a00e148e50c2c74f4e1d68
Zilong Zhong <zhongzl924@gmail.com>
2018-11-03 03:09:16 -0400

    Update pascal_voc.py

--- encoding/datasets/pascal_voc.py
+++ encoding/datasets/pascal_voc.py
@@ -16,7 +16,7 @@ class VOCSegmentation(BaseDataset):
     ]
     NUM_CLASS = 21
     BASE_DIR = 'VOCdevkit/VOC2012'
-    def __init__(self, root=os.path.expanduser('~/.encoding/data'), split='train',
+    def __init__(self, root=os.path.expanduser('/media/zilong/b368236f-2592-49f3-9dcc-0d4da674fd26/.encoding/data'), split='train',
                  mode=None, transform=None, target_transform=None, **kwargs):
         super(VOCSegmentation, self).__init__(root, split, mode, transform,
                                               target_transform, **kwargs)
